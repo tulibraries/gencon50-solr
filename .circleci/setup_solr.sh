a196b6f08baf201f7d83886762f2d481a7de188f
sensei100 <tuh42082@temple.edu>
2024-08-07 09:43:02 -0400

    Update make command

--- .circleci/setup_solr.sh
+++ .circleci/setup_solr.sh
@@ -5,7 +5,7 @@ git clone --single-branch --branch main git@github.com:tulibraries/ansible-playb
 mkdir -p ansible-playbook-solrcloud/data/tmp/collections
 cp -r ~/project ansible-playbook-solrcloud/data/tmp/collections/gencon50-solr
 cd ansible-playbook-solrcloud
-make build
+make build-lite
 STATUS=$(docker exec solr1 curl -s -o /dev/null -w "%{http_code}" http://solr1:8983/solr)
 while [[ "$STATUS" != "302" ]]; do
   echo waiting for setup to complete or equal 302.
