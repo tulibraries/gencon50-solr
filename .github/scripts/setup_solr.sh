1a6a3137841a0c23a110b989d072dbc938b98802
Chris Doyle <cdoyle@temple.edu>
2025-09-16 15:24:31 -0400

    move circleci config and scripts to github workflows

---
+++ .github/scripts/setup_solr.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+cd ..
+git clone --single-branch --branch main https://x-access-token:${GITHUB_TOKEN}@github.com/tulibraries/ansible-playbook-solrcloud.git
+mkdir -p ansible-playbook-solrcloud/data/tmp/collections
+cp -r $GITHUB_WORKSPACE ansible-playbook-solrcloud/data/tmp/collections/gencon50-solr
+cd ansible-playbook-solrcloud
+make build-lite
+STATUS=$(docker exec solr1 curl -s -o /dev/null -w "%{http_code}" http://solr:SolrRocks@solr1:8983/solr)
+while [[ "$STATUS" != "302" ]]; do
+  echo waiting for setup to complete or equal 302.
+  echo "currently: $STATUS"
+  sleep 2
+  STATUS=$(docker exec solr1 curl -s -o /dev/null -w "%{http_code}" http://solr:SolrRocks@solr1:8983/solr)
+done
+make create-deploy-collections
+make create-deploy-aliases
